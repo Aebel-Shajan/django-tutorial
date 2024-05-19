3639f0468563e4ec67c37971b7f3a89f5d0615cf
Aebel Shajan <67755450+Aebel-Shajan@users.noreply.github.com>
2024-05-19 14:27:49 +0100

    Fix missing self parameter in was_published_recently method

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -11,7 +11,7 @@ class Question(models.Model):
   def __str__(self) -> str:
     return self.question_text
   
-  def was_published_recently():
+  def was_published_recently(self):
     return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
