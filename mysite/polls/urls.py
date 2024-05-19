1e42e7cada454f7548876e4d6a91c067634ea71c
Aebel Shajan <67755450+Aebel-Shajan@users.noreply.github.com>
2024-05-19 18:55:44 +0100

    Refactor index.html template to use Django URL template tag for question links

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 
 from . import views
 
-
+# How does one make it so that Django knows which app 
+# view to create for a url when using the {% url %} template tag?
+# The answer is to add namespaces to your URLconf. 
+app_name = "polls"
 urlpatterns = [
     # ex: /polls/
     path("", views.index, name="index"),
